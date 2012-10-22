caab9d9843a016ccd45e57b79ad98519dfe34b9f
Szymon Urbaś <szymon.urbas@aol.com>
2012-10-22 20:03:05 +0200

    Add third option.

--- gra.c
+++ gra.c
@@ -24,16 +24,33 @@ int main(void)
 
   o("Zacząłeś grać w Grę.\n\n");
   o("[1] Wygrywasz Grę.\n");
-  o("[2] Przegrywasz Grę.\n\n");
+  o("[2] Przegrywasz Grę.\n");
+  o("[3] Bierzesz czerwoną pigułkę.\n\n");
 
   o("Co robisz? ");
   i();
 
   switch (in){
     case 1: o("\nWygrałeś Grę! Gratulacje!\n");
-            break;
+            return 0;
     case 2: o("\nJesteś frajerem.\n");
-            break;
+            return 1;
+    case 3: o("\nPostanawiasz wziąć czerwoną pigułkę. Czujesz się źle.\n\n");
+            o("[1] Idziesz rzygać do kibla.\n");
+            o("[2] Umierasz.\n");
+            o("[3] Zaczynasz czuć się dobrze.\n\n");
+
+            o("Co robisz? ");
+            i();
+
+            switch (in){
+              case 1: o("Rzygasz do kibla i wyrzygujesz płuca. Przegrałeś!\n");
+                      return 0;
+              case 2: o("Umierasz. Przegrałeś!\n");
+                      return 0;
+              case 3: o("Zaczynasz czuć się dobrze. Wygrałeś!\n");
+                      return 1;
+            }
   }
 
   return 0;
